4408f868fc1ace7c73237749111e86b8c0537d6f
starnivty <starnivty@gmail.com>
2026-04-19 19:59:03 +0700

    fix auth

--- api/register.js
+++ api/register.js
@@ -2,14 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
-  {
-    global: {
-      headers: {
-        Authorization: req.headers.authorization,
-      },
-    },
-  }
+  process.env.SUPABASE_ANON_KEY
 );
 
 function makePseudoEmail(username) {
