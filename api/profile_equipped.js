f13e9de97c8114bbce75d36a5573163b9e3e8753
starnivty <starnivty@gmail.com>
2026-04-26 18:59:50 +0700

    add api profile_equipped

---
+++ api/profile_equipped.js
@@ -0,0 +1,99 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default async function handler(req, res) {
+  const authHeader = req.headers.authorization || "";
+
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY,
+    {
+      global: {
+        headers: {
+          Authorization: authHeader,
+        },
+      },
+    }
+  );
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError || !user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  // GET profile milik user login
+  if (req.method === "GET") {
+    const { data, error } = await supabase
+        .from("player_profiles")
+        .select(`
+        *,
+        player_equipped!player_equipped_player_id_fkey (
+            right_arm:player_equipments!player_equipped_right_arm_fkey (
+            id,
+            level,
+            star,
+            catalog:catalog_equipments (
+                id,
+                name,
+                link_photo
+            )
+            ),
+            left_arm:player_equipments!player_equipped_left_arm_fkey (
+            id,
+            level,
+            star,
+            catalog:catalog_equipments (
+                id,
+                name,
+                link_photo
+            )
+            ),
+            lower:player_equipments!player_equipped_lower_fkey (
+            id,
+            level,
+            star,
+            catalog:catalog_equipments (
+                id,
+                name,
+                link_photo
+            )
+            ),
+            upper:player_equipments!player_equipped_upper_fkey (
+            id,
+            level,
+            star,
+            catalog:catalog_equipments (
+                id,
+                name,
+                link_photo
+            )
+            ),
+            first_partner:player_partners!player_equipped_first_partner_fkey (
+            id,
+            level,
+            star,
+            partner_id
+            ),
+            second_partner:player_partners!player_equipped_second_partner_fkey (
+            id,
+            level,
+            star,
+            partner_id
+            )
+        )
+        `)
+        .eq("player_id", user.id)
+        .single();
+
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+    return res.status(200).json({ data });
+    }
+
+  return res.status(405).json({ error: "Method not allowed" });
+}
